d081bc13ece085dc87edd125a5aecdc38366cfd2
tenk-a <tenka@6809.net>
2018-11-27 00:22:35 +0900

    borland-c/c++ 用調整

--- borland/string.h
+++ borland/string.h
@@ -9,6 +9,8 @@
 #if __BORLANDC__ >= 0x700
  #include <../crtl/string.h>
 #else
+ #include __CCWRAP_NATIVE_C_HEADER_PATH(limits.h)
+ #include __CCWRAP_NATIVE_C_HEADER_PATH(float.h)
  #include __CCWRAP_NATIVE_C_HEADER_PATH(string.h)
  #if defined(__cplusplus) && __BORLANDC__ < 0x561
   using std::strlen;
@@ -22,11 +24,11 @@
 static inline char* stpcpy(char* d, char const* s) { return _stpcpy(d, s); }
 #endif
 
-static inline int   strcasecmp(char const* l, char const* r) { return stricmp(l, r); }
-static inline int   strncasecmp(char const* l, char const* r, size_t n) { return strnicmp(l, r, n); }
+static inline int	strcasecmp(char const* l, char const* r) { return stricmp(l, r); }
+static inline int	strncasecmp(char const* l, char const* r, size_t n) { return strnicmp(l, r, n); }
 #if __BORLANDC__ >= 0x560
-static inline int   wcscasecmp(wchar_t const* l, wchar_t const* r) { return _wcsicmp(l, r); }
-static inline int   wcsncasecmp(wchar_t const* l, wchar_t const* r, size_t n) { return _wcsnicmp(l, r, n); }
+static inline int	wcscasecmp(wchar_t const* l, wchar_t const* r) { return _wcsicmp(l, r); }
+static inline int	wcsncasecmp(wchar_t const* l, wchar_t const* r, size_t n) { return _wcsnicmp(l, r, n); }
 #endif
 
 
