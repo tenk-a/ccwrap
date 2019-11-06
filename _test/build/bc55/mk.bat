bab5c57fd209d32aae874aabc087fc0ed0d350e1
tenk-a <tenka@6809.net>
2019-11-07 08:37:42 +0900

    __int8 -> char

--- _test/build/bc55/mk.bat
+++ _test/build/bc55/mk.bat
@@ -12,9 +12,12 @@ if exist smp_c.exe del smp_c.exe
 
 bcc32 -I%SRCDIR% -I%INCDIR% -I%INCDIR2% -I%BOOSTDIR% %SRCDIR%\smp.cpp
 bcc32 -I%SRCDIR% -I%INCDIR% -I%INCDIR2% -I%BOOSTDIR% %SRCDIR%\smp_c.c
-bcc32 -DCOMPILING_FOR_ERROR -I%SRCDIR% -I%INCDIR% -I%INCDIR2% -I%BOOSTDIR% %SRCDIR%\smp.cpp
 
 if exist smp.exe smp.exe
 if exist smp_c.exe smp_c.exe
+
+echo -
+echo [COMPILE ERROR CHECK]
+bcc32 -DCOMPILING_FOR_ERROR -I%SRCDIR% -I%INCDIR% -I%INCDIR2% -I%BOOSTDIR% %SRCDIR%\smp.cpp
 
 endlocal
--- borland/stdint.h
+++ borland/stdint.h
@@ -20,8 +20,8 @@
 #include __CCWRAP_NATIVE_C_HEADER_PATH(stdint.h)
 #else
 #include <_stddef.h>
-typedef __int8	    	    int8_t;
-typedef unsigned __int8     uint8_t;
+typedef char	    	    int8_t;
+typedef unsigned char		uint8_t;
 typedef __int16     	    int16_t;
 typedef unsigned __int16    uint16_t;
 typedef __int32     	    int32_t;
