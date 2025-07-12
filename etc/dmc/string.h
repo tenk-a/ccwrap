d316378dcd7aff0cb81316a677f6376ace83e76d
tenk-a <tenka@6809.net>
2025-07-12 21:47:33 +0900

    etc 側 static inline 修正.

--- etc/dmc/string.h
+++ etc/dmc/string.h
@@ -6,12 +6,12 @@
 #include <../include/string.h>
 
 #if 1
-inline char*	strdup(char const* s) { return _strdup(s); }
+static inline char*    strdup(char const* s) { return _strdup(s); }
 //inline char* strupr(char* p) { return _strupr(s); }
 //inline char* strlwr(char* p) { return _strlwr(s); }
 
-inline int  strcasecmp(char const* l, char const* r) { return _stricmp(l, r); }
-inline int  strncasecmp(char const* l, char const* r, size_t n) { return _strnicmp(l, r, n); }
-inline int  wcscasecmp(wchar_t const* l, wchar_t const* r) { return _wcsicmp(l, r); }
-inline int  wcsncasecmp(wchar_t const* l, wchar_t const* r, size_t n) { return _wcsnicmp(l, r, n); }
+static inline int  strcasecmp(char const* l, char const* r) { return _stricmp(l, r); }
+static inline int  strncasecmp(char const* l, char const* r, size_t n) { return _strnicmp(l, r, n); }
+static inline int  wcscasecmp(wchar_t const* l, wchar_t const* r) { return _wcsicmp(l, r); }
+static inline int  wcsncasecmp(wchar_t const* l, wchar_t const* r, size_t n) { return _wcsnicmp(l, r, n); }
 #endif
